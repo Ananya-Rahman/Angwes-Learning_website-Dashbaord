de89f34c942ffcd7933f6ffce228620494bbedfd
Ananya-Rahman <ananya35-1689@diu.edu.bd>
2021-12-20 23:20:01 +0600

    add full design

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function($){
+    "use strict";
+    $("[data-background").each(function(){
+        $(this).css("background-image", "url(" + $(this).attr("data-background")+")")
+     });
+
+       // Toggle the side navigation
+  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+    $("body").toggleClass("sidebar-toggled");
+    $(".sidebar").toggleClass("toggled");
+    if ($(".sidebar").hasClass("toggled")) {
+      $('.sidebar .collapse').collapse('hide');
+    };
+  });
+
+    // Close any open menu accordions when window is resized below 768px
+    $(window).resize(function() {
+        if ($(window).width() < 768) {
+          $('.sidebar .collapse').collapse('hide');
+        };
+        
+        // Toggle the side navigation when window is resized below 480px
+        if ($(window).width() < 480 && !$(".sidebar").hasClass("toggled")) {
+          $("body").addClass("sidebar-toggled");
+          $(".sidebar").addClass("toggled");
+          $('.sidebar .collapse').collapse('hide');
+        };
+      });
+    
+
+    //  var menuIcon = document.querySelector(".menu-icon");
+    //  var sidebar = document.querySelector(".sidebar");
+    //  var contentWrapper = document.querySelector(".contentWrapper");
+     
+    //  menuIcon.onclick = function () {
+    //      sidebar.classList.toggle("small-sidebar");
+    //      contentWrapper.classList.toggle("large-container");
+     
+    //  }
+      
+     
+}(jQuery));
